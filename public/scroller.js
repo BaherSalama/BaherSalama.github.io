e0a586a259ba326cbe3470ffdcd7a79e24435fa5
BaherSalama <blacktiger70047@gmail.com>
2025-08-03 23:54:14 +0300

    fix workflow

--- public/scroller.js
+++
@@ -1,8 +0,0 @@
-let a = document.getElementById("scroller")
-if (a){
-  let count = a.children.length
-  for (let i=0;i<count;i++){
-    let b = a.children[i].cloneNode(true);
-    a.appendChild(b);
-  }
-}
